d188a91706442c1d349e224d9723f1d1b50f6e9d
PseudoFunc <sudipta0102@gmail.com>
2024-05-11 18:54:17 +0530

    a wrapper class added

---
+++ src/test/java/com/pseudofunc/util/Constants.java
@@ -0,0 +1,18 @@
+package com.pseudofunc.util;
+/*
+It's a wrapper class. Main objective is to create wrapper for the keys in config and xml file.
+ */
+public class Constants {
+
+    public static final String GRID_ENABLED = "selenium-grid-enabled";
+    public static final String GRID_URL_FORMAT = "selenium-grid-urlFormat";
+    public static final String GRID_HUB_HOST = "selenium-grid-hubhost";
+
+    public static final String BROWSER = "browser";
+    public static final String BROWSER_CHROME = "chrome";
+    public static final String BROWSER_FIREFOX = "firefox";
+
+    public static final String FLIGHT_RESERVATION_URL = "flightReservationURL";
+    public static final String VENDOR_PORTAL_URL = "vendorPortalURL";
+
+}
